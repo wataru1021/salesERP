254110b9982a55892b48de3eab87c3757c094b0d
fu89.team <fu89.info@gmail.com>
2021-07-16 10:22:11 +0700

    fix revision

--- backend/app/Http/Controllers/Sales/UsersController.php
+++ backend/app/Http/Controllers/Sales/UsersController.php
@@ -6,8 +6,12 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\Admin\AdminLoginRequest;
 use App\Enums\RoleStateType;
 use App\Models\User;
+use Carbon\Carbon;
+use Exception;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 
 class UsersController extends Controller
 {
@@ -16,20 +20,21 @@ class UsersController extends Controller
      *
      * @return void
      */
-   
 
-    public function index(Request $request) {
+
+    public function index(Request $request)
+    {
         if (Auth::guard('sales')->check()) {
             return redirect(route('home'));
         }
         return view('sales.users.login');
-    }   
+    }
 
     public function login(Request $request)
     {
         $credentials = $request->only('email', 'password');
         $credentials['role_id'] = RoleStateType::SALER;
-        
+
         $message = '';
         if (Auth::guard('sales')->attempt($credentials)) {
             return redirect(route('home'));
@@ -42,15 +47,60 @@ class UsersController extends Controller
         ]);
     }
 
-    public function forgotPassword(Request $request) {
-
+    public function forgotPassword(Request $request)
+    {
     }
 
-    public function register(Request $request) {
+    public function register(Request $request)
+    {
         return view('sales.users.register');
     }
 
-    public function registerPost(Request $request) {
-    }
+    public function registerPost(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => ['unique:users'],
+        ]);
+        if ($validator->fails()) {
+            return view('sales.users.register', [
+                'messageEmail' => 'メールは既に存在します',
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => $request->password
+            ]);
+        }
 
+        try {
+            $user =  User::insert([
+                'name' => $request->name,
+                'email' => $request->email,
+                'role_id' => RoleStateType::SALER,
+                'password' => Hash::make($request->password),
+                'company_id' => 1,
+                'created_at' => Carbon::now()->toDateTimeString(),
+            ]);
+
+            $credentials = $request->only('email', 'password');
+            $credentials['role_id'] = RoleStateType::SALER;
+
+            $message = '';
+            if (Auth::guard('sales')->attempt($credentials)) {
+                return redirect(route('home'));
+            } else {
+                return view('sales.users.register', [
+                    'message' => 'アカウント登録に失敗しました',
+                    'name' => $request->name,
+                    'email' => $request->email,
+                    'password' => $request->password
+                ]);
+            }
+        } catch (Exception $e) {
+            return view('sales.users.register', [
+                'message' => 'アカウント登録に失敗しました',
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => $request->password
+            ]);
+        }
+    }
 }
--- backend/resources/views/sales/Users/register.blade.php
+++ backend/resources/views/sales/Users/register.blade.php
@@ -5,5 +5,10 @@
             :form-url="{{ json_encode(route('register.post')) }}"
             :login-url="{{ json_encode(route('login')) }}"
             :message="{{ json_encode(isset($message) ? $message : '') }}"
+            :message-email="{{ json_encode(isset($messageEmail) ? $messageEmail : '') }}"
+            :name-user="{{ json_encode(isset($name) ? $name : '') }}"
+            :email-user="{{ json_encode(isset($email) ? $email : '') }}"
+            :password-user="{{ json_encode(isset($password) ? $password : '') }}"
     ></sales-register>
+
 @endsection
